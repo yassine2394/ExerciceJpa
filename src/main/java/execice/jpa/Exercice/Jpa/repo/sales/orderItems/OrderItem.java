51971d1d3a7b8c263f8a6910b7cd461b52356f56
hamza-nejib <hamza-nejib@outlook.com>
2024-09-19 17:52:18 +0200

    implémentation des classes : Order et OrderItem

--- src/main/java/execice/jpa/Exercice/Jpa/repo/sales/orderItems/OrderItem.java
+++ src/main/java/execice/jpa/Exercice/Jpa/repo/sales/orderItems/OrderItem.java
@@ -1,4 +1,37 @@
 package execice.jpa.Exercice.Jpa.repo.sales.orderItems;
 
+import execice.jpa.Exercice.Jpa.repo.production.products.Product;
+import execice.jpa.Exercice.Jpa.repo.sales.orders.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "order_Items",schema = "sales")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class OrderItem {
+    @Id
+    @Column(name = "item_id")
+    int itemId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    Product product;
+
+    @Column(name = "quantity")
+    int quantity;
+
+    @Column(name = "list_price")
+    double listPrice;
+
+    @Column(name = "discount")
+    double discount;
+
 }
--- src/main/java/execice/jpa/Exercice/Jpa/repo/sales/orders/Order.java
+++ src/main/java/execice/jpa/Exercice/Jpa/repo/sales/orders/Order.java
@@ -1,8 +1,47 @@
 package execice.jpa.Exercice.Jpa.repo.sales.orders;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import execice.jpa.Exercice.Jpa.repo.sales.customers.Customer;
+import execice.jpa.Exercice.Jpa.repo.sales.staffs.Staff;
+import execice.jpa.Exercice.Jpa.repo.sales.stores.Store;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDate;
+
+@Entity
 @Table(name = "orders",schema = "sales")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Order {
+    @Id
+    @Column(name = "order_id")
+    int orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false)
+    Store store;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id", nullable = false)
+    Staff staff;
+
+    @Column(name="order_status")
+    String orderStatus;
+
+    @Column(name="order_date")
+    LocalDate orderDate;
+
+    @Column(name="required_date")
+    LocalDate requiredDate;
+
+    @Column(name="shipped_date")
+    LocalDate shippedDate;
+
 }
